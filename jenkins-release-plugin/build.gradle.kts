71beebc914abf0d6facbacf5d27e9cbf39478334
Yg0R2 <yg0r2@yahoo.com>
2022-01-20 18:33:28 +0100

    feature: add release-jenkins-plugin skeleton

---
+++ jenkins-release-plugin/build.gradle.kts
@@ -0,0 +1,43 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("eazy-kotlin-library-conventions") version("+")
+    id("org.jenkins-ci.jpi") version("+")
+}
+
+jenkinsPlugin {
+    jenkinsVersion.set("2.319")
+    displayName = "Eazy Release Plugin"
+    fileExtension = "jpi"
+    pluginFirstClassLoader = true
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType(KotlinCompile::class.java).all {
+    dependsOn("localizer")
+
+    kotlinOptions {
+        sourceCompatibility = "1.8"
+        targetCompatibility = "1.8"
+
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(project(":eazy-release-gradle-plugin"))
+
+    // Jenkins BOM
+    implementation(platform("io.jenkins.tools.bom:bom-2.319.x:+"))
+
+    compileOnly("org.jenkins-ci.plugins", "plugin", "4.12")
+    compileOnly("org.jenkins-ci.plugins.workflow", "workflow-step-api")
+    compileOnly("org.jenkins-ci.plugins.workflow", "workflow-job")
+
+    // Test service dependencies
+    testImplementation("org.jenkinsci.plugins", "pipeline-model-definition")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,13 +15,20 @@ pluginManagement {
     }
 }
 
+// Core
 include("eazy-release-plugin-core")
 project(":eazy-release-plugin-core").projectDir = file("core")
 
+// Gradle
 include("eazy-release-gradle-plugin")
 project(":eazy-release-gradle-plugin").projectDir = file("gradle-release-plugin")
 
 include("gradle-release-plugin-acceptance-test")
 
+// Jenkins
+include("eazy-release-jenkins-plugin")
+project(":eazy-release-jenkins-plugin").projectDir = file("jenkins-release-plugin")
+
+// Maven
 include("eazy-release-maven-plugin")
 project(":eazy-release-maven-plugin").projectDir = file("maven-release-plugin")
