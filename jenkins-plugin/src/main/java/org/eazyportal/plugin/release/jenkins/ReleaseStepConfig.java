614ea87edde984942a7ca63bcf41b055cf74c00e
Yg0R2 <yg0r2@yahoo.com>
2023-09-18 09:54:40 +0200

    feature: use JGit in Jenkins plugin

--- jenkins-plugin/src/main/java/org/eazyportal/plugin/release/jenkins/ReleaseStepConfig.java
+++ jenkins-plugin/src/main/java/org/eazyportal/plugin/release/jenkins/ReleaseStepConfig.java
@@ -1,9 +1,8 @@
 package org.eazyportal.plugin.release.jenkins;
 
 import hudson.model.InvisibleAction;
-import org.eazyportal.plugin.release.core.executor.CliCommandExecutor;
 import org.eazyportal.plugin.release.core.scm.ConventionalCommitType;
-import org.eazyportal.plugin.release.core.scm.GitActions;
+import org.eazyportal.plugin.release.core.scm.JGitActions;
 import org.eazyportal.plugin.release.core.scm.ScmActions;
 import org.eazyportal.plugin.release.core.scm.model.ScmConfig;
 
@@ -13,7 +12,7 @@ import java.util.List;
 public class ReleaseStepConfig extends InvisibleAction implements Serializable {
 
     private final transient List<ConventionalCommitType> conventionalCommitTypes = ConventionalCommitType.getDEFAULT_TYPES();
-    private final transient ScmActions scmActions = new GitActions(new CliCommandExecutor());
+    private final transient ScmActions scmActions = new JGitActions();
     private final transient ScmConfig scmConfig = ScmConfig.getGIT_FLOW();
 
     public List<ConventionalCommitType> getConventionalCommitTypes() {
--- jenkins-plugin/src/test/java/org/eazyportal/plugin/release/jenkins/ReleaseStepConfigTest.java
+++ jenkins-plugin/src/test/java/org/eazyportal/plugin/release/jenkins/ReleaseStepConfigTest.java
@@ -1,7 +1,7 @@
 package org.eazyportal.plugin.release.jenkins;
 
 import org.eazyportal.plugin.release.core.scm.ConventionalCommitType;
-import org.eazyportal.plugin.release.core.scm.GitActions;
+import org.eazyportal.plugin.release.core.scm.JGitActions;
 import org.eazyportal.plugin.release.core.scm.model.ScmConfig;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,8 +24,8 @@ class ReleaseStepConfigTest {
         // THEN
         assertThat(underTest.getConventionalCommitTypes()).isEqualTo(ConventionalCommitType.getDEFAULT_TYPES());
         assertThat(underTest.getScmActions())
-            .isInstanceOf(GitActions.class)
-            .isNotNull();
+                .isInstanceOf(JGitActions.class)
+                .isNotNull();
         assertThat(underTest.getScmConfig()).isEqualTo(ScmConfig.getGIT_FLOW());
     }
 
